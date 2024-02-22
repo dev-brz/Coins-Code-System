e443d26d78c63018bc22249d0bd76fbd5e75a0d7
brcgtdv0 <160502200+brcgtdv0@users.noreply.github.com>
2024-02-22 10:38:38 +0100

    chore: initialize back-end application [#7]

---
+++ coins-code-api/src/main/java/com/cgzt/coinscode/CoinsCodeApplication.java
@@ -0,0 +1,13 @@
+package com.cgzt.coinscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinsCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinsCodeApplication.class, args);
+    }
+
+}
---
+++ coins-code-api/src/test/java/com/cgzt/coinscode/CoinsCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgzt.coinscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinsCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
