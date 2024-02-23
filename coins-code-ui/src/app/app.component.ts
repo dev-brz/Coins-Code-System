9d1b4e05f91a8987cdaef4855cfc0580c79d4e8e
brcgtdv0 <160502200+brcgtdv0@users.noreply.github.com>
2024-02-23 15:09:40 +0100

    config: add i18n [#8] (#16)

--- coins-code-ui/src/app/app.component.ts
+++ coins-code-ui/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID, isDevMode } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,9 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  constructor(@Inject(LOCALE_ID) locale: string) {
+    if (isDevMode()) {
+      console.info(`Using locale: ${locale}`);
+    }
+  }
 }
